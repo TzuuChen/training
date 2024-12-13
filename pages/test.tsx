992d8d294ff0b3dbe578aa5ce8f42fb6c66b535a
TzuuChen <jf76501@gmail.com>
2024-12-13 13:00:58 +0800

    fix(styles) resolve CSS specificity issue

--- pages/test.tsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-	return (
-		<div className="container">
-			<h1 className="form_textField">About Page</h1>
-			<p>This is the about page.</p>
-		</div>
-	);
-}
